f3a3152b6f7dae9745129326bfe1e7becff25430
mensahAmpomah <gideonmensah201@gmail.com>
2025-05-21 01:02:56 -0700

    Assignment on Calculating simple Interest

---
+++ python_introduction/simple_interest.py
@@ -0,0 +1,18 @@
+# An App for calculating simple interest on a given principal
+# amount,rate of interest, and time.
+
+# I will be creating the variables below.
+
+# This variable is for the princial amount
+principal = 1000
+
+# This varibale is for  the rate percentage
+rate = 0.05
+
+# This variable is Time
+time = 3
+
+interest = principal * rate * time 
+
+print(f'The simple interest is : {interest}')
+
