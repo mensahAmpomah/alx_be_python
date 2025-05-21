01308d8e440142844c1ac360c2cc132f76da9d73
mensahAmpomah <gideonmensah201@gmail.com>
2025-05-21 00:49:30 -0700

    Assignment on performing Basic Arithmetic Operations

---
+++ python_introduction/basic_operations.py
@@ -0,0 +1,11 @@
+# This program is to practice basic arithmetic operations
+# in Python by Performing predefined calculations.
+
+# Here's for creating variables for both arithmetic operations
+
+number1 = 10 
+number2 = 5
+
+print(f'Addition of {number1} and {number2} is {number1 + number2}')
+print(f'Subtraction of {number1} and {number2} is {number1 - number2}')
+print(f'Multiplication of {number1} and {number2} is {number1 * number2}')
