45562d2da8b72c5d052234d3437fde6775d88b50
mensahAmpomah <gideonmensah201@gmail.com>
2025-05-22 18:38:44 -0700

    Assignment on Personal finance calculator complete

---
+++ python_introduction/finace_calculator.py
@@ -0,0 +1,11 @@
+# An App for Personal finance Calculator.
+
+monthly_income = int(input('Enter your monthly income: '))
+monthly_expenses = int(input('Enter your monthly expenses: '))
+
+monthly_savings = monthly_income - monthly_expenses
+
+Projected_savings = monthly_savings * 12 + (monthly_savings * 12 * 0.05)
+
+print(f'Your monthly savings are {monthly_savings}')
+print(f'Projected savings after one year, with interest, is : {Projected_savings}')
