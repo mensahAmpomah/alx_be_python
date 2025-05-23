857773ac51018c1a702be71e4e8e51841f2dc4af
mensahAmpomah <gideonmensah201@gmail.com>
2025-05-22 17:14:05 -0700

    Deleted the variable number_of_mins_sec

--- python_introduction/hours_to_seconds.py
+++ python_introduction/hours_to_seconds.py
@@ -1,8 +1,7 @@
 # An App for Converting a specific number of hours into seconds
 
-hours = 2
-number_of_min_sec = 60 * 60
+hours = 2 * 60 * 60
 
 # Variable for storing the seconds answer
-seconds = hours * number_of_min_sec
+seconds = hours 
 print(f'{hours} hours(s) is {seconds} seconds.')
