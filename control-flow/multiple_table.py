02339b3aced6d1284797a2d3786c3e2df6253af8
mensahAmpomah <gideonmensah201@gmail.com>
2025-05-30 06:55:34 -0700

    Multiplication Table completed

---
+++ control-flow/multiple_table.py
@@ -0,0 +1,9 @@
+# Multiplication Table Generator.
+
+number = int(input("Enter a number to see its multiplication: "))
+
+# the nested loop 
+for i in list[number]:
+    # print(number)
+    for j in range(1,11):
+        print(f"{number} * {j} = {number*j}")
