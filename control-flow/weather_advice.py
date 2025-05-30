aaa0419efeb988d841196f823243f150ba5df6d9
mensahAmpomah <gideonmensah201@gmail.com>
2025-05-29 22:28:12 -0700

    Completed Weather Advice App

---
+++ control-flow/weather_advice.py
@@ -0,0 +1,14 @@
+# Weather advice app
+
+# Creating the weather variable that prompts the user
+weather = input("What's the weather like today?(sunny/rainy/cold)")
+
+# using the if to check what the user input
+if weather.lower() == 'sunny':
+    print('Wear a t-shirt and sunglasses.')
+elif weather.lower() == 'rainy':
+    print("Don't forget your umbrella and a raincoat.")
+elif weather.lower() == 'cold':
+    print("Make sure to wear a warm coat and a scarf.")
+else:
+    print("Sorry, I don't have recommendations for this weather.")
