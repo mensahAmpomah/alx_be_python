d6c14e3c20d40eef9d8261cbef3d1f0739e97919
mensahAmpomah <gideonmensah201@gmail.com>
2025-05-30 07:39:43 -0700

    Change the list

--- control-flow/multiple_table.py
+++ control-flow/multiple_table.py
@@ -3,7 +3,7 @@
 number = int(input("Enter a number to see its multiplication: "))
 
 # the nested loop 
-for i in list[number]:
+for i in [number]:
     # print(number)
     for j in range(1,11):
         print(f"{number} * {j} = {number*j}")
