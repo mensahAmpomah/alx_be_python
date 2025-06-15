225159cf103c1391421eac6600595137eb62e0b3
mensahAmpomah <gideonmensah201@gmail.com>
2025-06-15 09:31:36 -0700

    Banck Account assignment completed

---
+++ programming_paradigm/bank_account.py
@@ -0,0 +1,25 @@
+# Defining the class named BankAccount.
+
+class BankAccount:
+    def __init__(self,initial_balance):
+        self.account_balance = initial_balance
+
+    def deposit(self,amount):
+        if amount > 0:
+            self.account_balance += amount
+            print(f"Deposited: ${amount}")
+        else:
+            print("Deposit amount must be positive.")
+
+    def withdraw(self,amount):
+        if amount <= self.account_balance:
+            self.account_balance -= amount
+            print(f"Withdrawn: ${amount}")
+            return True
+        else:
+            print("Insufficient funds. Withdrawal failed.")
+            return False
+        
+    def display_balance(self):
+        print(f"Current Balance: ${self.account_balance}")
+        
