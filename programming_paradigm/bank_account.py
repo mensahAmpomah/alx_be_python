a6ecef45c19c43a3672854f85e1b4b7815ba6d96
mensahAmpomah <gideonmensah201@gmail.com>
2025-06-15 09:54:51 -0700

    Set the initial balance to zero

--- programming_paradigm/bank_account.py
+++ programming_paradigm/bank_account.py
@@ -1,7 +1,7 @@
 # Defining the class named BankAccount.
 
 class BankAccount:
-    def __init__(self,initial_balance):
+    def __init__(self,initial_balance = 0):
         self.account_balance = initial_balance
 
     def deposit(self,amount):
