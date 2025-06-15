c0471a8878bb0853d935e8a3fd926c1f7249ec2a
mensahAmpomah <gideonmensah201@gmail.com>
2025-06-15 10:39:08 -0700

    Robust Calculator Completed

---
+++ programming_paradigm/robust_division_calculator.py
@@ -0,0 +1,13 @@
+# Robust Calculator
+
+def safe_divide(numerator,denominator):
+    try:
+        num = float(numerator)
+        den = float(denominator)
+
+        result = num / den
+        return f"Result: {result}"
+    except ZeroDivisionError:
+        return "Error: Cannot divide by zero."
+    except ValueError:
+        return "Error: Please enter numeric values only."
