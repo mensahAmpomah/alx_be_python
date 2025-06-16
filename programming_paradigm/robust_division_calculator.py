610b71eda6152815fcf58af0070fb75d75631884
mensahAmpomah <gideonmensah201@gmail.com>
2025-06-15 18:49:37 -0700

    Change result to 'The result of the division is'

--- programming_paradigm/robust_division_calculator.py
+++ programming_paradigm/robust_division_calculator.py
@@ -6,7 +6,7 @@ def safe_divide(numerator,denominator):
         den = float(denominator)
 
         result = num / den
-        return f"Result: {result}"
+        return f"The result of the division is {result}"
     except ZeroDivisionError:
         return "Error: Cannot divide by zero."
     except ValueError:
